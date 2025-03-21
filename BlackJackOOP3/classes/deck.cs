ae2aaec655972ad23661eadbe65a14cc36803eda
S-Heijnemans <153827872+S-Heijnemans@users.noreply.github.com>
2025-03-21 13:21:55 +0100

    add-actions-deck

--- BlackJackOOP3/classes/deck.cs
+++ BlackJackOOP3/classes/deck.cs
@@ -9,5 +9,10 @@ namespace BlackJackOOP3.classes
     class Deck
     {
         List<Card> cards;
+
+        public void RandomizeDeck()
+        {
+            //this is to randomize the cards in the deck without having douplecates of any of the cards
+        }
     }
 }
